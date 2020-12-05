e4b519b2b0b71af5498553497854cda0c6973294
= <=>
2020-12-05 21:57:10 +0700

    commit first

---
+++ src/components/body/home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './home.css'
+
+function Home() {
+    return (
+        <div className="home_page">
+            <h2>Welcome to FakeZalo</h2>
+            
+        </div>
+    )
+}
+
+export default Home
